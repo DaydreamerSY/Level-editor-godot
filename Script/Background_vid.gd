b27b9a069855aed0d70a67fabefa15ee091f6f43
DaydreamerSY <sakurayami1999@gmail.com>
2023-08-02 14:48:09 +0700

    temporary disabled live wallpaper

--- Script/Background_vid.gd
+++ Script/Background_vid.gd
@@ -14,24 +14,26 @@ var current_video_time = 0
 var time_now
 var time_start
 
+var is_play = false
+
 var tween
 var old_vid
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	bg_1 = $Background_vid
-	bg_2 = $Background_vid_2
-	
-	bg_1.stream = load(_video)
-	bg_2.stream = load(_video)
-	bg_2.play()
-	current_vid_playing = 2
-	
-	if _fade_delay == 0:
-		bg_2.connect("finished", bg_2.play)
-		pass
-	
-	time_start = Time.get_unix_time_from_system()
+#	bg_1 = $Background_vid
+#	bg_2 = $Background_vid_2
+#
+#	bg_1.stream = load(_video)
+#	bg_2.stream = load(_video)
+#	bg_2.play()
+#	current_vid_playing = 2
+#
+#	if _fade_delay == 0:
+#		bg_2.connect("finished", bg_2.play)
+#		pass
+#
+#	time_start = Time.get_unix_time_from_system()
 	pass # Replace with function body.
 
 
@@ -39,16 +41,16 @@ func _ready():
 func _process(delta):
 #	if not player.is_playing():
 #		player.play()
-	time_now = Time.get_unix_time_from_system()
-	var elapsed = round(time_now - time_start)
+#	time_now = Time.get_unix_time_from_system()
+#	var elapsed = round(time_now - time_start)
 #	var minutes = elapsed / 60
 #	var seconds = int(fmod(elapsed , 60))
 #	var str_elapsed = "%02d : %02d" % [minutes, seconds]
 #	print("elapsed : ", elapsed)
 	
 
-	if elapsed >= (_vid_length - _fade_delay):
-		_switch_video()
+#	if elapsed >= (_vid_length - _fade_delay):
+#		_switch_video()
 #		print("Current video playing: %s" % [current_vid_playing])
 
 	pass
@@ -108,6 +110,8 @@ func _move_to_behind():
 			current_vid_playing = 1
 			pass
 	pass
+
+
 
 func _load_ogv(path):
 	bg_1.stream = load(path)
